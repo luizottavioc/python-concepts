aaa905a37e8ce7102ea83495018ddbd6e6b61bbe
Luiz Otávio Carvalho <luizottavio49@gmail.com>
2022-05-28 14:52:32 -0300

    0 numbers corresponding condition

--- RandomNumbers.py
+++ RandomNumbers.py
@@ -7,7 +7,7 @@ for i in range(100):
   vet.append(random.randint(0,100))
 print("Your array has already been incremented with 100 random numbers!")
 
-n = int(input("Now, enter the integer number you want to find in array:"))
+n = int(input("Now, enter the integer number you want to find in array: (0-100)"))
 for i in range(len(vet)):
   if(vet[i] == n):
     corresp.append(i)
@@ -18,4 +18,5 @@ print("------------------")
 print("Array:\n"+str(vet))
 print("------------------")
 print("Count of times your number was found: "+str(qtCorresp))
-print("Positions where the number was found: "+ str(corresp))
+if(qtCorresp != 0):
+  print("Positions where the number was found: "+ str(corresp))
